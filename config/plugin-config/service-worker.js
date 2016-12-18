25370746577baf9ad355339f4cbca3cc86b0c43a
Gilly Ames <gilly.ames@gmail.com>
2016-12-18 18:24:10 +0000

    Image loads offline

--- config/plugin-config/service-worker.js
+++ config/plugin-config/service-worker.js
@@ -1,16 +1,11 @@
 module.exports = {
   cacheId: 'my-project-name',
-  filename: 'static/js/my-service-worker.js',
+  filename: 'my-service-worker.js',
   maximumFileSizeToCacheInBytes: 4194304,
   runtimeCaching: [{
     urlPattern: /https?:\/\/fonts.+/,
     handler: 'fastest'
   }],
-  staticFileGlobs: [
-     "build/index.html",
-     "build/static/js/**.js",
-     "build/images/**/**.*"
-  ],
   verbose: true,
   staticFileGlobsIgnorePatterns: [/js.map/],
   navigateFallback: 'index.html',
