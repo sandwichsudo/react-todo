0cfdd5ac2d1c4ccad63beb91876e5b6f72330283
Gilly Ames <gilly.ames@gmail.com>
2016-12-18 17:38:31 +0000

    Making stuff work offline

--- config/plugin-config/service-worker.js
+++ config/plugin-config/service-worker.js
@@ -1,11 +1,17 @@
 module.exports = {
   cacheId: 'my-project-name',
-  filename: 'my-service-worker.js',
+  filename: 'static/js/my-service-worker.js',
   maximumFileSizeToCacheInBytes: 4194304,
   runtimeCaching: [{
-    handler: 'cacheFirst',
-    urlPattern: /[.]mp3$/,
+    urlPattern: /https?:\/\/fonts.+/,
+    handler: 'fastest'
   }],
+  staticFileGlobs: [
+     "build/index.html",
+     "build/static/js/**.js",
+     "build/images/**/**.*"
+  ],
+  verbose: true,
   staticFileGlobsIgnorePatterns: [/js.map/],
   navigateFallback: 'index.html',
   navigateFallbackWhitelist: [/^\/login/, /^\/shop/, /^\/tab/, /^\/admin/]
