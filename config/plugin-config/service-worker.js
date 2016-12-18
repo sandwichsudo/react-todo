f041740d51b8d9785f51d8504941f43872e1be3b
Gilly Ames <gilly.ames@gmail.com>
2016-12-18 21:34:40 +0000

    Fix title and add images to runtime cache

--- config/plugin-config/service-worker.js
+++ config/plugin-config/service-worker.js
@@ -1,13 +1,19 @@
 module.exports = {
-  cacheId: 'my-project-name',
-  filename: 'my-service-worker.js',
-  maximumFileSizeToCacheInBytes: 4194304,
-  runtimeCaching: [{
-    urlPattern: /https?:\/\/fonts.+/,
-    handler: 'fastest'
-  }],
-  verbose: true,
-  staticFileGlobsIgnorePatterns: [/js.map/],
-  navigateFallback: 'index.html',
-  navigateFallbackWhitelist: [/^\/login/, /^\/shop/, /^\/tab/, /^\/admin/]
+    cacheId: 'my-project-name',
+    filename: 'my-service-worker.js',
+    maximumFileSizeToCacheInBytes: 4194304,
+    runtimeCaching: [{
+        urlPattern: /https?:\/\/fonts.+/,
+        handler: 'fastest'
+    }, {
+        urlPattern: /https?:\/\/img.tesco+/,
+        handler: 'fastest'
+    }, {
+        urlPattern: /https?:\/\/lh5.googleusercontent.com+/,
+        handler: 'fastest'
+    }],
+    verbose: true,
+    staticFileGlobsIgnorePatterns: [/js.map/],
+    navigateFallback: 'index.html',
+    navigateFallbackWhitelist: [/^\/login/, /^\/shop/, /^\/tab/, /^\/admin/]
 };
