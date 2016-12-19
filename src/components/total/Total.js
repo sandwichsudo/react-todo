d218c849f2165ac7c410cdb6aeae52a92b181516
vivaldi-va <wildtangomedia@gmail.com>
2016-12-19 10:07:50 +0000

    total styles

--- src/components/total/Total.js
+++ src/components/total/Total.js
@@ -3,6 +3,11 @@ import React from 'react';
 // Using "Stateless Functional Components"
 export default function(props) {
     return (
-        <h4>Total is £ { Number(props.total).toFixed(2) }</h4>
+        <div className="total-wrapper">
+            <div className="total-header">Total</div>
+            <div className="total-value">
+               £{ Number(props.total).toFixed(2) }
+           </div>
+        </div>
     );
 }
