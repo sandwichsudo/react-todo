9f78cdca975bde3cf8ce41845e0b8b299a027a0b
Gilly Ames <gilly.ames@gmail.com>
2016-12-19 13:50:43 +0000

    Admin page styling

--- src/views/authenticated/admin/Admin.js
+++ src/views/authenticated/admin/Admin.js
@@ -4,41 +4,47 @@ import React from 'react';
 export default function(props) {
     return (
         <div>
-            { props.user && props.user.isAdmin && <div className="main-container">
-                <p>As an admin, you can add more items to the catalogue:</p>
-                <form onSubmit={ props.handleSubmit } >
-                    <label>
-                        <span className="label">Product name</span>
-                        <input
-                            className="input"
-                            onChange={ props.handleProdNameChange }
-                            value={ props.prodName }
-                        />
-                    </label>
-                    <label>
-                        <span className="label">Product cost</span>
-                        <input
-                            className="input"
-                            type="number"
-                            onChange={ props.handleProdCostChange }
-                            value={ props.prodCost }
-                        />
-                    </label>
-                <button className="primary-button">Add</button>
-                </form >
-            </div>}
-            <h2>This is a list of which users owe what:</h2>
-            <ul className="list"> { props.usersList &&
+            { props.user && props.user.isAdmin &&
+                <div className="main-container">
+                <h1 className="in-app-title">Add items to the shop</h1>
+                    <form className="admin-form" onSubmit={ props.handleSubmit } >
+                        <label>
+                            <span className="visuallyhidden">Product name</span>
+                            <input
+                                placeholder="Product name"
+                                className="input"
+                                onChange={ props.handleProdNameChange }
+                                value={ props.prodName }
+                            />
+                        </label>
+                        <label>
+                            <span className="visuallyhidden">Product cost</span>
+                            <input
+                                placeholder="Product cost"
+                                className="input"
+                                type="number"
+                                onChange={ props.handleProdCostChange }
+                                value={ props.prodCost }
+                            />
+                        </label>
+                        <button className="primary-button">Add</button>
+                    </form >
+                </div>}
+            <div className="main-container">
+                <h2 className="in-app-title">Who owes what</h2>
+            </div>
+            <ul className="list admin-list"> { props.usersList &&
                 Object.keys(props.usersList).map(key => (
                     <li key={ key } >
-                        <div className="list-item-wrapper">
+                         { props.calculateTotal(props.usersList[key]) !== 0 && <div className="list-item-wrapper">
                             <span className="name">{ props.usersList[key].displayName }</span>
                             <span className="owes">£ { props.calculateTotal(props.usersList[key]).toFixed(2) }</span>
-                        </div>
+                        </div>  }
                     </li>
                 ))
             }
             </ul>
+
         </div>
     );
 }
