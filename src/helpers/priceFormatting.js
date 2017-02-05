f2e0f80d23ba5d37a78b9a9640ce6415031e8d33
Gilly Ames <gilly.ames@gmail.com>
2017-02-05 22:25:53 +0000

    formatting prices for < £1

--- src/helpers/priceFormatting.js
+++ src/helpers/priceFormatting.js
@@ -1,5 +1,8 @@
 module.exports = {
     formatPrice: (priceInPence) => {
+        if (0 < priceInPence && priceInPence < 100) {
+            return `${(Number(priceInPence))}p`;
+        }
         return `£${(Number(priceInPence)/100).toFixed(2)}`;
     },
 }
