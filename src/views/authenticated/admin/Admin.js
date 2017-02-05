41c542f1b8c26d11407570063387174ee1746888
Gilly Ames <gilly.ames@gmail.com>
2017-02-05 18:05:59 +0000

    Updated database rules so that no one can edit teams, and made the admin of a team a property of that team. removed admin ability to add products to team data base for now.

--- src/views/authenticated/admin/Admin.js
+++ src/views/authenticated/admin/Admin.js
@@ -5,32 +5,6 @@ import { formatPrice } from '../../../helpers/priceFormatting';
 export default function(props) {
     return (
         <div>
-            { props.user && props.user.isAdmin &&
-                <div className="main-container">
-                <h1 className="in-app-title">Add items to the shop</h1>
-                    <form className="admin-form" onSubmit={ props.handleSubmit } >
-                        <label>
-                            <span className="visuallyhidden">Product name</span>
-                            <input
-                                placeholder="Product name"
-                                className="input"
-                                onChange={ props.handleProdNameChange }
-                                value={ props.prodName }
-                            />
-                        </label>
-                        <label>
-                            <span className="visuallyhidden">Product cost</span>
-                            <input
-                                placeholder="Product cost"
-                                className="input"
-                                type="number"
-                                onChange={ props.handleProdCostChange }
-                                value={ props.prodCost }
-                            />
-                        </label>
-                        <button className="primary-button">Add</button>
-                    </form >
-                </div>}
             <div className="main-container">
                 <h2 className="in-app-title">Who owes what</h2>
             </div>
