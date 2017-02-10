0deeb14e7d8e10d55592adc6414c03c34d71f4fd
Gilly Ames <gilly.ames@gmail.com>
2017-02-10 16:46:08 +0000

    reorganised login form

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -5,19 +5,23 @@ import Google from 'react-icons/fa/google-plus';
 export default function(props) {
     return (
         <div className="form-wrap">
-            {<form>
+            {<div>
                 <h1 className="title">Tuckshop</h1>
                 <h2 className="subtitle">Hungry? login now</h2>
-                <label>
-                    <span className="hidden-label">Email</span>
-                    <input value={props.email} onChange={props.handleEmailChange} className="input email-input" placeholder="Email"/>
-                </label>
-                <label>
-                    <span className="hidden-label">Password</span>
-                    <input value={props.password} onChange={props.handlePasswordChange} type="password" className="input password-input" placeholder="Password"/>
-                </label>
+                <form>
+                   <label>
+                       <span className="hidden-label">Email</span>
+                       <input value={props.email} onChange={props.handleEmailChange} className="input email-input" placeholder="Email"/>
+                    </label>
+                    <label>
+                       <span className="hidden-label">Password</span>
+                       <input value={props.password} onChange={props.handlePasswordChange} type="password" className="input password-input" placeholder="Password"/>
+                    </label>
+                    <div className="button-wrap">
+                       <button type="button" onClick={props.emailPasswordLogin} className="primary-button">Sign Up / Login</button>
+                    </div>
+                </form>
                 <div className="button-wrap">
-                    <button type="button" onClick={props.emailPasswordLogin} className="primary-button">Sign Up / Login</button>
                     <div className="divider"><span className="or">OR</span></div>
                     <button className="social-button primary-button" onClick={props.facebookLogin}>
                         <Facebook className="icon"></Facebook>
@@ -29,7 +33,7 @@ export default function(props) {
                     </button>
                 </div>
                 {props.usualProvider && <h3>Hmm.. looks like you usually log in with your {props.usualProvider} account. Why not use that instead?</h3>}
-            </form>}
+            </div>}
         </div>
     );
 }
